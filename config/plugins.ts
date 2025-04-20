9d143a66e37674ef21c194515402590878965d60
Anas Marzuki <muhammadanasmarz@gmail.com>
2025-04-20 23:47:53 +0800

    adding cloudinary plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,25 @@
-export default () => ({});
+export default ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDNAME'),
+          api_key: env('CLOUDAPIKEY'),
+          api_secret: env('CLOUDINARYSECRET'),
+        },
+        actionOptions: {
+          upload: {
+            use_filename: true,
+            unique_filename: false,
+            overwrite: false,
+            folder: 'test-folder',
+            resource_type: 'auto',
+            // Add the following configuration to enforce folder structure
+            public_id: (_, resource) => `${resource?.folder ?? 'test-folder'}/${resource?.originalFilename}`,
+          },
+          delete: {},
+        },
+      },
+    },
+  });
+  
